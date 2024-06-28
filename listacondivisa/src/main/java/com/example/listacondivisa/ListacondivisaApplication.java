a8404352cbc77193c0845d6d84787722450ca009
C4rra <168854623+C4rra@users.noreply.github.com>
2024-06-28 17:52:09 +0200

    progetto

---
+++ listacondivisa/src/main/java/com/example/listacondivisa/ListacondivisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.listacondivisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListacondivisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListacondivisaApplication.class, args);
+	}
+
+}
---
+++ listacondivisa/src/test/java/com/example/listacondivisa/ListacondivisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listacondivisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListacondivisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
